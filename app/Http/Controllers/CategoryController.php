817c9ffe053954fbd86fcfd00a0b527670096e67
Coder Khayrul <coderkhayrul@gmail.com>
2021-06-01 20:21:53 +0600

    Product Category Store In Database

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CategoryController extends Controller
 {
@@ -16,6 +18,13 @@ class CategoryController extends Controller
         return view('admin.categories');
     }
     public function savecategory(Request $request) {
-
+        // dd($request->category_name);
+        $checkcategory = Category::where('category_name', $request->category_name)->first();
+        if (!$checkcategory) {
+            Category::create($request->all());
+            return redirect()->route('category.create')->with('status', 'The'.$request->category_name.' Category has been saved successfully');
+        }else {
+            return redirect()->route('category.create')->with('status1', 'The '.$request->category_name.' Category already exist');
+        }
     }
 }
--- resources/views/admin/addcategory.blade.php
+++ resources/views/admin/addcategory.blade.php
@@ -10,6 +10,19 @@ Add Category
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title">Create Category</h4>
+                <!-- Get Session Status  Start-->
+                @if (Session::has('status') )
+                <div class="alert alert-success text-white">
+                    {{ Session::get('status') }}
+                </div>
+                @endif
+                @if (Session::has('status1') )
+                <div class="alert alert-danger text-white">
+                    {{ Session::get('status1') }}
+                </div>
+                @endif
+                <!-- Get Session Status End -->
+
                 {!! Form::open(['route' =>'category.store', 'class' => 'cmxform', 'method' => 'post', 'id' =>
                 'commentForm']) !!}
                 @csrf
